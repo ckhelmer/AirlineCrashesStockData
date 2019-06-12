ec600d6de07aba2c7dca170edff5af56740a75a6
CK Helmer <caitlinhelmer@gmail.com>
2019-06-11 20:38:57 -0400

    complete flask app!

--- app.py
+++ app.py
@@ -1,73 +1,107 @@
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify 
+from flask import Flask, jsonify, render_template
 
 ################################
 #### Connection to Database ####
 ################################
 
+# Create our session (link) from Python to the DB
 engine = create_engine("sqlite:///airplanestocks.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-print(Base)
-
-# Save reference to the table
-AmericanCrashes = Base.classes.american_crashes
-
-AllCrashes = Base.classes.all_crashes
-
-# Create our session (link) from Python to the DB
 session = Session(engine)
+# reflect an existing database into a new model
+Base = declarative_base()
 
+#Declare the bases because automap doesn't work
+class AmericanCrashes(Base):
+    __tablename__ = "american_crashes"
+    id = Column(Integer, primary_key = True)
+    date = Column(String(10))
+    manufacturer = Column(String(255))
+    carrier = Column(String(255))
+    fatalities = Column(Integer)
+    location = Column(String(255))
+ 
+class AllCrashes(Base):
+    __tablename__ = "all_crashes"
+    id = Column(Integer, primary_key = True)
+    date = Column(String(10))
+    manufacturer = Column(String(255))
+    carrier = Column(String(255))
+    fatalities = Column(Integer)
+    location = Column(String(255)) 
 
 ####################################
 ############# Flask ################
 ####################################
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/api')
+def api():
     return(f"Available Routes:<br/>"
         f"/api/v1.0/american_carriers <br/>"
         f"/api/v1.0/all_carriers")
 
 @app.route('/api/american_carriers')
-def crashes():
-    results = session.query(AmericanCrashes.date, AmericanCrashes.manufacturer, AmericanCrashes.carrier, AmericanCrashes.fatalities, AmericanCrashes.location)
+def american_crashes():
+    engine = create_engine("sqlite:///airplanestocks.sqlite")
+
+    session = Session(engine)
     
+    results = session.query(AmericanCrashes.date, AmericanCrashes.manufacturer, AmericanCrashes.carrier, AmericanCrashes.fatalities, AmericanCrashes.location).all()
     american_carriers = []
-    for date, manufacturer, carrier, fatalities, location in results:
+    for result in results:
         american_dict = {}
-        american_dict['date'] = date
-        american_dict['manufacturer'] = manufacturer
-        american_dict['carrier'] = carrier
-        american_dict['fatalities'] = location
+        american_dict['date'] = result.date
+        american_dict['manufacturer'] = result.manufacturer
+        american_dict['carrier'] = result.carrier
+        american_dict['fatalities'] = result.fatalities
+        american_dict['location'] = result.location
         american_carriers.append(american_dict)
         
-        return jsonify(american_carriers)
+    return jsonify(american_carriers)
 
 
-#@app.route('/api/all_carriers')
-#def crashes():
-    #results = session.query(AllCrashes.date, AllCrashes.manufacturer, AllCrashes.carrier, AllCrashes.fatalities, AllCrashes.location)
+@app.route('/api/all_carriers')
+def all_crashes():
+    engine = create_engine("sqlite:///airplanestocks.sqlite")
+
+    session = Session(engine)
     
-    #all_carriers = []
-    #for date, manufacturer, carrier, fatalities, location in results:
-        #airline_dict = {}
-        #airline_dict['date'] = date
-        #airline_dict['manufacturer'] = manufacturer
-        #airline_dict['carrier'] = carrier
-        #airline_dict['fatalities'] = location
-        #airline_carriers.append(airline_dict)
+    results = session.query(AllCrashes.date, AllCrashes.manufacturer, AllCrashes.carrier, AllCrashes.fatalities, AllCrashes.location).all()
+    
+    airline_carriers = []
+    for date, manufacturer, carrier, fatalities, location in results:
+        airline_dict = {}
+        airline_dict['date'] = date
+        airline_dict['manufacturer'] = manufacturer
+        airline_dict['carrier'] = carrier
+        airline_dict['fatalities'] = fatalities
+        airline_dict['location'] = location
+        airline_carriers.append(airline_dict)
         
-        #return jsonify(airline_carriers)    
-    
+    return jsonify(airline_carriers)    
+
+@app.route('/stock_impact')
+def stock_impact():
+    return render_template('stock_impact.html', name=name)
+
+@app.route('/crash')
+def crash():    
+    return render_template('crash.html', name=name)
+
+@app.route('/datastock')
+def datastock():
+    return render_template('data-stock.html', name=name)
+
+@app.route('/')
+def home():
+    return render_template('index.html', name=name)
     
 if __name__ == '__main__':
     app.run(debug=True)    
